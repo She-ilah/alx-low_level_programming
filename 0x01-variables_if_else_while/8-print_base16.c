dfaddcd51152b11449cdb82515ce1ef1c472147c
She-ilah <sheilahmutale1@gmail.com>
2023-03-19 13:51:18 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 48;
+	int k = 97;
+
+		while (n <= 57)
+		{
+			putchar(n);
+			n++;
+		}
+		while (k <= 102)
+		{
+			putchar(k);
+			k++;
+		}
+		putchar('\n');
+		return (0);
+}
