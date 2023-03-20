93740adc566951273847142c259034a405deea86
She-ilah <sheilahmutale1@gmail.com>
2023-03-20 04:52:36 -0700

    task 12 changes

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,32 +9,25 @@ int main(void)
 {
 	int a;
 	int b;
-	int c;
-	int d;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b = 0; b < 9; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
-			for (c = 0; c < 10; c++)
-			{
-				for (d = 1; d < 10; d++)
-				{
-				putchar(a + '0');
-				putchar(b + '0');
-				putchar(' ');
-				putchar(c + '0');
-				putchar(d + '0');
-				if (a == 9 && b == 8 && c == 9 && d == 9)
-				{
-				continue;
-				}
-				putchar(',');
-				putchar(' ');
-				}
-			}
+		putchar((a / 10) + '0');
+		putchar((a % 10) + '0');
+		putchar(' ');
+		putchar((b / 10) + '0');
+		putchar((b % 10) + '0');
+		if (a == 98 && b == 99)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
 		}
 	}
 	putchar('\n');
 	return (0);
+
 }
