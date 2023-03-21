55d957318a4e5aa326ef87046b0dc72aa3b41155
She-ilah <sheilahmutale1@gmail.com>
2023-03-21 16:29:43 -0700

    task 11

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints the n times table
+ *
+ * Return: void
+ */
+int main(void)
+{
+	print_times_table(3)
+	_putchar('\n');
+	print_times_table(5)
+	_putchar('\n');
+	print_times_table(98)
+	_putchar('\n');
+	print_times_table(12)
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,17 +15,11 @@ void print_to_98(int n)
 	{
 	if (n == 98)
 	{
-		printf("%d", n);
-		printf(",");
-		printf(" ");
-		printf("\n");
 		break;
 	}
 	else
 	{
-		printf("%d", n);
-		printf(",");
-		printf(" ");
+		printf(", %d", n);
 	}
 	}
 	}
@@ -35,17 +29,13 @@ void print_to_98(int n)
 	{
 	if (n == 98)
 	{
-		printf("%d", n);
-		printf(",");
-		printf(" ");
+		printf(", %d", n);
 		printf("\n");
 		break;
 	}
 	else
 	{
-		printf("%d", n);
-		printf(",");
-		printf(" ");
+		printf(", %d", n);
 	}
 	}
 	}
