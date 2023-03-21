713281b20fc00a4663e002137fa319e68e392820
She-ilah <sheilahmutale1@gmail.com>
2023-03-21 16:02:12 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even-valued terms
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	long m = 1, p = 2, sum = p;
+
+	while (p + m < 4000000)
+	{
+	p += m;
+	if (p % 2 == 0)
+	sum += p;
+	++n;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
