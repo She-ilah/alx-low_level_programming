c67552d362bffeac395ecb3b0b7726643b61c759
She-ilah <sheilahmutale1@gmail.com>
2023-04-11 07:45:47 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - creates an array of chars
+ * @size: number of buytes
+ * @c: char that initializes array
+ * Return: char, pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int n;
+
+	ar = malloc(sizeof(char) * size);
+	
+	if (size == 0 || ar == NULL)
+	{
+		return(NULL);
+	}
+	for (n = 0; n < size; n++)
+	{
+		ar[n] = c;
+	}
+	return(ar);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,4 @@
 int _putchar(char c);
-void *malloc(size_t size);
-void free(void *ptr);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
