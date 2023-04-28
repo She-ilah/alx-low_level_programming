b5f9e6b48dac3c3a6fd327000f12e30617279e41
She-ilah <sheilahmutale1@gmail.com>
2023-04-28 13:00:45 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void first_func(void)__attribute__((constructor));
+
+/**
+ * first - prints a given statement
+ */
+void first_func(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
