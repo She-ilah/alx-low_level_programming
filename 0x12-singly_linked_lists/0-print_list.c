88bd844752f44c7f5afb3f9bd0654d354ac89846
She-ilah <sheilahmutale1@gmail.com>
2023-04-28 13:18:29 -0700

    task 0 changes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,17 +11,17 @@ size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (!h->str)
+		if (h->str == NULL)
 		{
-			printf("[0](nil)\n");
+			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%u] %s\n", h -> len, h -> str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
-		h = h -> next;
+		h = h->next;
 		n++;
 	}
 	return (n);
