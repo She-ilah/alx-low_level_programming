cb97e0dbb2047baa78dd40406eb8fb2709c4c355
She-ilah <sheilahmutale1@gmail.com>
2023-04-28 13:09:36 -0700

    task 0 fixes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,9 +19,9 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h -> len, h -> str);
 		}
-		h = h->next;
+		h = h -> next;
 		n++;
 	}
 	return (n);
