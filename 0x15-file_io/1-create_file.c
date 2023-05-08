e55708d392ffaf39057dfce47d1221803d9b0f90
She-ilah <sheilahmutale1@gmail.com>
2023-05-08 14:48:49 -0700

    changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,8 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int len;
-	int fptr, num;
+	int len, fptr, num;
 
 	if (filename == NULL)
 		return (-1);
@@ -27,5 +26,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	close(fptr);
+
 	return (1);
 }
