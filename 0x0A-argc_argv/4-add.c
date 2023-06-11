d7244ffc4403d0884b7dd7e736490899f50252d7
She-ilah <sheilahmutale1@gmail.com>
2023-06-11 02:50:38 -0700

    Changes to file 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+
+int is_num(char *str);
+
+/**
+ * is_num - Verifies that given string contains only integers
+ * @str: String to be checked
+ * Return: Always 0
+ */
+int is_num(char *str)
+{
+	unsigned int num = 0;
+
+	while (num < strlen(str))
+	{
+		if (!isdigit(str[num]))
+		{
+			return (0);
+		}
+		num++;
+	}
+	return (1);
+}
 
 /**
  * main - Program adds positive numbers
@@ -11,23 +34,26 @@
  */
 int main(int argc, char *argv[])
 {
-	unsigned int sum = 0;
-	int i;
+	int num;
+	int str;
+	int sum = 0;
 
-	for (i = 1; i < argc; i++)
+	num = 1;
+	while (num < argc)
 	{
-		if (atoi(argv[i]) >= '0' && atoi(argv[i]) <= '9')
+		if (is_num(argv[num]))
 		{
-			sum += atoi(argv[i]);
+			str = atoi(argv[num]);
+			sum += str;
 		}
 		else
 		{
 			printf("Error\n");
 			return (1);
 		}
+		num++;
 	}
 	printf("%d\n", sum);
 
 	return (0);
 }
-
