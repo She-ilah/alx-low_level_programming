db16b048502be6febd90ae34c744e53eca6a7cca
She-ilah <sheilahmutale1@gmail.com>
2023-06-11 00:51:46 -0700

    task 3- Program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program that multiplies two numbers
+ * @argc: Argument count
+ * @argv: Argument Vectors
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1, mul = 1;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		mul = mul * atoi(argv[i]);
+	}
+	printf("%d\n", mul);
+	return (0);
+}
