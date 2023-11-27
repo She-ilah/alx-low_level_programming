baa840d01c303f9864b78c002600a756e7ddd6ac
She-ilah <sheilahmutale1@gmail.com>
2023-11-27 05:18:56 -0800

    error check

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,23 +7,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
-	hash_node_t **array;
+	hash_table_t *ht;
 	unsigned long int n;
 
-	table = malloc(sizeof(hash_table_t));
-	if (table == NULL)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
 
-	array = malloc(sizeof(hash_node_t *) * size);
-	if (array == NULL)
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) *
+			size);
+	if (ht->array == NULL)
 		return (NULL);
-
 	for (n = 0; n < size; n++)
-		array[n] = NULL;
+		ht->array[n] = NULL;
 
-	table->array = array;
-	table->size = size;
-
-	return (table);
+	return(ht);
 }
